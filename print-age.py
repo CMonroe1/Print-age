a0e41f5d60d6a87f15a97d2fc20374f663b88692
CMonroe1 <choicey1@twc.com>
2016-09-04 21:19:25 -0500

    Initial commit

---
+++ print-age.py
@@ -0,0 +1,7 @@
+age = input('Enter your age: ')
+#age_in_seconds = age * 365 * 24 * 60 * 60
+#print("You have lived for " + str(int(age) * 365 * 24 * 60 * 60) + " seconds")
+#print("you have lived for {} seconds.".format(str(int(age) * 365 * 24 * 60 * 60)))
+#print("you have lived for {} seconds.".format(int(age) * 365 * 24 * 60 * 60)))
+print("You have lived for {} seconds. This corresponds to {} years".format(int(age) * 365 * 24 * 60 * 60, age))
+
